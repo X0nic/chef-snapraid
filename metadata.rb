2c9480cb04667960609a2ebb1096d0fe491eff77
Sam Martin <samjackmartin@gmail.com>
2015-04-15 23:41:09 +0100

    Initial update  - Parameterised SnapRAID version  - Updated default SnapRAID version to latest (7.1)  - Updated kitchen to latest two Ubuntu LTS versions  - Added ark::default to default recipe to ensure dependencies are installed  - Added apt::default to kitchen (could not get ark dependencies to install without it)  - Removed :configure from ark install of SnapRAID (prevented :install_with_make from running due to the remote_file step common to both already being up-to-date. Bug in chef_ark?)  - Kitchen test now passes

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures snapraid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends 'ark'
+depends 'ark', "= 0.9.0"
+depends 'apt'
