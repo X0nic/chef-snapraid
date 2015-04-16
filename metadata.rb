e37f68671aba73b0e8aac46766faee8e230a6eef
Sam Martin <samjackmartin@gmail.com>
2015-04-16 07:30:19 +0100

    Removed apt from metadata (already in kitchen.yml for testing) Removed version lock troubleshooting step on ark

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      'Installs/Configures snapraid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends 'ark', "= 0.9.0"
-depends 'apt'
+depends 'ark'
