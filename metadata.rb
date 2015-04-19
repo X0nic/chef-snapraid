3f78d45a2976c7e14fa63d51244654df3365be6a
Sam Martin <samjackmartin@gmail.com>
2015-04-19 19:14:45 +0100

    Updated license from cookbook default to Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'snapraid'
 maintainer       'Nathan Lee'
 maintainer_email 'nathan@globalphobia.com'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Installs/Configures snapraid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
