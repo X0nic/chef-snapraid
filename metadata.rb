6da4f55af545fe47d95e446518db96d89dd7da03
Nathan Lee <nathan@globalphobia.com>
2015-04-23 22:20:54 -0600

    bump for release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'nathan@globalphobia.com'
 license          'Apache v2.0'
 description      'Installs/Configures snapraid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 depends 'ark'
