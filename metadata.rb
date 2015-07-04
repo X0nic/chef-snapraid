aab0717b6df0d0dc4d7d13ebda411f7175976711
Sam Martin <samjackmartin@gmail.com>
2015-07-04 10:10:12 +0100

    Fixed tab vs spaces

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache v2.0'
 description      'Installs/Configures snapraid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.1'
-supports		'ubuntu'
-supports		'centos'
+supports         'ubuntu'
+supports         'centos'
 
-depends			'ark'
+depends          'ark'
